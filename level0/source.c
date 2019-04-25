873c2bf9635d775d81100eed548d07cf72b02a36
Kevin COSTA <kcosta@e1r4p7.42.fr>
2019-04-25 13:04:15 +0200

    level0

---
+++ level0/source.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#define _GNU_SOURCE
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+  char *sh;
+  gid_t gid;
+  uid_t uid;
+  char *arg[2];
+
+  if (atoi(argv[1]) == 423)
+  {
+    sh = strdup("/bin/sh");
+
+    gid = getegid();
+    uid = geteuid();
+
+    setresgid(gid, gid, gid);
+    setresuid(uid, uid, uid);
+
+    arg[0] = "sh";
+    arg[1] = 0;
+    execv("/bin/sh", arg);
+  }
+  else
+    write(1, "No !\n", 5);
+}
